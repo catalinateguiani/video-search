50107f53f717372875e110d8c28eba079f798599
Catalina Teguiani <35691248+catalinateguiani@users.noreply.github.com>
2019-08-19 15:00:42 +0100

    Update youtube.js

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const KEY = "AIzaSyAGzE6mmT0BHw22xSoNMkrCLGzgmGA2-Pc";
+const KEY = "";
 
 export default axios.create({
   baseURL: "https://www.googleapis.com/youtube/v3",
